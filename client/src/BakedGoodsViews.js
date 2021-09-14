ae1a3a4c519ccb12dd3068192bc05b04fbbea8f4
Eric Sanchez <ericfsod@gmail.com>
2021-09-14 18:54:37 -0400

    Backend funtional. Mapped through bakeditems array and displayed on page with button

--- client/src/BakedGoodsViews.js
+++ client/src/BakedGoodsViews.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 function BakedGoodsViews() {
-	// const [ bakeItem, setBakeItem ] = useState([]);
+	const [ bakeItems, setBakeItems ] = useState([]);
 	useEffect(() => {
 		fetch('/bakeries').then((response) => response.json()).then((bakedData) => {
-			console.log(bakedData);
+			setBakeItems(bakedData);
 		});
 	}, []);
 
-	return <div>hi</div>;
+	return bakeItems.map((bakeItem) => {
+		return (
+			<div className="bake-item">
+				<img src={bakeItem.image} alt="A delicious donut" />
+				<p>{bakeItem.note}</p>
+				<button onClick={() => handleDelete()}>BAM</button>
+			</div>
+		);
+	});
+
+	function handleDelete() {
+		console.log('hi');
+		return <div />;
+	}
 }
 
 export default BakedGoodsViews;
